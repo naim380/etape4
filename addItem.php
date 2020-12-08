2fb975df6fd588c759fec528c482d55269fa6868
naim380 <naim.ebrahim@le-campus-numerique.fr>
2020-12-08 16:54:41 +0100

    mise à jour

--- addItem.php
+++ addItem.php
@@ -1,20 +1,17 @@
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    
-</body>
-</html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
+<form method="post" action="displayItem.php">
+<p><label>prenom : <input type="texte" name="prenom"/></label>
+ 
+ 
+ <p><input type="submit"/></p>
+     
+  
+ </form>
 </body>
 </html>
